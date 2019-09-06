7feb45bcdff937bd9a02ab9e9c37a408826a2e6f
Matheus Mesquita <matheus.santos@ambar.tech>
2019-09-06 11:58:55 -0300

    Added Movie model

---
+++ app/src/main/java/matheus/liberproject/model/Models.kt
@@ -0,0 +1,5 @@
+package matheus.liberproject.model
+
+data class Movie(val name: String, val released: String, val imdbRating: Float,
+                 val nationality: String, val synopsis: String, val cover: String,
+                 val duration: String, val evaluation: String)
