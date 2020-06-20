f48bfaac70cd95e8d8b2caa0745f2f8d79db0bf4
Matheus Mesquita <matheus.santos@ambar.tech>
2020-06-20 20:17:08 -0300

    Added movie detail model

--- app/src/main/java/matheus/liberproject/model/Models.kt
+++ app/src/main/java/matheus/liberproject/model/Models.kt
@@ -4,3 +4,33 @@ data class MovieResponse(val Search: List<Movie>, val Response: String, val tota
 
 data class Movie(val Title: String, val Year: String, val imdbID: String, val Type: String,
                  val Poster: String)
+
+data class Ratings(val Source: String, val Value: String)
+
+data class MovieDetails(
+    val Title: String,
+    val Year: String,
+    val Rated: String,
+    val Released: String,
+    val Runtime: String,
+    val Genre: String,
+    val Director: String,
+    val Writer: String,
+    val Actors: String,
+    val Plot: String,
+    val Language: String,
+    val Country: String,
+    val Awards: String,
+    val Poster: String,
+    val Ratings: List<Ratings>,
+    val Metascore: String,
+    val imdbRating: String,
+    val imdbVotes: String,
+    val imdbID: String,
+    val Type: String,
+    val DVD: String,
+    val BoxOffice: String,
+    val Production: String,
+    val Website: String,
+    val Response: String
+)
