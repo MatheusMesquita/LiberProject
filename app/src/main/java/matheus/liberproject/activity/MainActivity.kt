525b977e2db197aabf1963ba49887f0f19e6f126
Matheus Mesquita <matheus.santos@ambar.tech>
2020-06-21 18:36:11 -0300

    Fixed list display

--- app/src/main/java/matheus/liberproject/activity/MainActivity.kt
+++ app/src/main/java/matheus/liberproject/activity/MainActivity.kt
@@ -3,8 +3,10 @@ package matheus.liberproject.activity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import matheus.liberproject.R
+import matheus.liberproject.adapter.CustomDividerItem
 import matheus.liberproject.adapter.MovieListAdapter
 import matheus.liberproject.model.Movie
 import matheus.liberproject.model.MovieResponse
@@ -34,6 +36,9 @@ class MainActivity : AppCompatActivity() {
         mAdapter.movies = mMovies
         rvMoviesList.adapter = mAdapter
         rvMoviesList.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this@MainActivity)
+        rvMoviesList.addItemDecoration(
+            CustomDividerItem(ContextCompat.getDrawable(this, R.drawable.divider)!!)
+        )
 
         swipeRefresh.setOnRefreshListener { callMovies() }
     }
---
+++ app/src/main/java/matheus/liberproject/adapter/CustomDividerItem.kt
@@ -0,0 +1,60 @@
+package matheus.liberproject.adapter
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.roundToInt
+
+/**
+ * Item decoration to insert a divider between Recycler View items
+ *
+ * @author Matheus Mesquita
+ * @property mDivider Drawable
+ * @property mBounds Rect
+ * @constructor
+ */
+class CustomDividerItem(private val mDivider: Drawable) : RecyclerView.ItemDecoration() {
+
+    private val mBounds = Rect()
+
+    override fun onDraw(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        canvas.save()
+        val left: Int
+        val right: Int
+        if (parent.clipToPadding) {
+            left = parent.paddingLeft
+            right = parent.width - parent.paddingRight
+            canvas.clipRect(
+                left, parent.paddingTop, right,
+                parent.height - parent.paddingBottom
+            )
+        } else {
+            left = 0
+            right = parent.width
+        }
+
+        val childCount = parent.childCount
+        for (i in 0 until childCount - 1) {
+            val child = parent.getChildAt(i)
+            parent.getDecoratedBoundsWithMargins(child, mBounds)
+            val bottom = mBounds.bottom + child.translationY.roundToInt()
+            val top = bottom - mDivider.intrinsicHeight
+            mDivider.setBounds(left, top, right, bottom)
+            mDivider.draw(canvas)
+        }
+        canvas.restore()
+    }
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+
+        when {
+            parent.getChildAdapterPosition(view) == state.itemCount - 1 ->
+                outRect.set(0, 18, 0, 18)
+            parent.getChildAdapterPosition(view) == 0 ->
+                outRect.set(0, 18, 0, mDivider.intrinsicHeight + 18)
+            else -> outRect.set(0, 18, 0, mDivider.intrinsicHeight + 18)
+        }
+    }
+}
